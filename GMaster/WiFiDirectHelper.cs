0575645c2758636f4dc382956ae25c5a1b2ef2fa
Rambalac <rambalac+github@gmail.com>
2017-03-10 10:49:27 +0900

    WiFiDirect

--- GMaster/WiFiDirectHelper.cs
+++ GMaster/WiFiDirectHelper.cs
@@ -5,92 +5,30 @@ using System.Text;
 using System.Threading.Tasks;
 using Windows.Devices.Enumeration;
 using Windows.Devices.WiFiDirect;
+using Windows.Networking.Proximity;
 using Microsoft.VisualBasic.CompilerServices;
 
 namespace GMaster
 {
     public class WiFiDirectHelper
     {
-        private WiFiDirectConnectionListener _listener;
-        private WiFiDirectAdvertisementPublisher _publisher;
-        private DeviceWatcher _deviceWatcher;
 
-        public WiFiDirectHelper()
+        public void Start()
         {
+            PeerFinder.ConnectionRequested += PeerFinder_ConnectionRequested;
+            PeerFinder.Start();
         }
 
-        public WiFiDirectAdvertisementPublisherStatus Start()
+        private void PeerFinder_ConnectionRequested(object sender, ConnectionRequestedEventArgs args)
         {
-            _publisher = new WiFiDirectAdvertisementPublisher();
-            _listener = new WiFiDirectConnectionListener();
-
-            _publisher.StatusChanged += OnStatusChanged; ;
-            try
-            {
-                // This can raise an exception if the machine does not support WiFi. Sorry.
-                _listener.ConnectionRequested += OnConnectionRequested;
-            }
-            catch (Exception ex)
-            {
-                return WiFiDirectAdvertisementPublisherStatus.Aborted;
-            }
-
-            _publisher.Advertisement.ListenStateDiscoverability = WiFiDirectAdvertisementListenStateDiscoverability.Normal;
-            _publisher.Advertisement.IsAutonomousGroupOwnerEnabled = true;
-            _publisher.Start();
-
-            var deviceSelector = WiFiDirectDevice.GetDeviceSelector(WiFiDirectDeviceSelectorType.AssociationEndpoint);
-
-            _deviceWatcher = DeviceInformation.CreateWatcher(deviceSelector, new string[] { "System.Devices.WiFiDirect.InformationElements" });
-
-            _deviceWatcher.Added += OnDeviceAdded;
-            _deviceWatcher.Removed += OnDeviceRemoved;
-            _deviceWatcher.Updated += OnDeviceUpdated;
-            _deviceWatcher.EnumerationCompleted += OnEnumerationCompleted;
-            _deviceWatcher.Stopped += OnStopped;
-
-            _deviceWatcher.Start();
-
-            return _publisher.Status;
+           // throw new NotImplementedException();
         }
 
-        private void OnStopped(DeviceWatcher sender, object args)
-        {
-        }
-
-        private void OnEnumerationCompleted(DeviceWatcher sender, object args)
-        {
-        }
-
-        private void OnDeviceUpdated(DeviceWatcher sender, DeviceInformationUpdate args)
-        {
-        }
-
-        private void OnDeviceRemoved(DeviceWatcher sender, DeviceInformationUpdate args)
-        {
-        }
-
-        private void OnDeviceAdded(DeviceWatcher sender, DeviceInformation args)
-        {
-        }
 
         public void Stop()
         {
-            _publisher.Stop();
-            _publisher.StatusChanged -= OnStatusChanged;
-
-            _listener.ConnectionRequested -= OnConnectionRequested;
+            PeerFinder.ConnectionRequested -= PeerFinder_ConnectionRequested;
+            PeerFinder.Stop();
         }
-
-        private void OnStatusChanged(WiFiDirectAdvertisementPublisher sender, WiFiDirectAdvertisementPublisherStatusChangedEventArgs args)
-        {
-            
-        }
-
-        private void OnConnectionRequested(WiFiDirectConnectionListener sender, WiFiDirectConnectionRequestedEventArgs args)
-        {
-            
-        }
-
     }
 }
